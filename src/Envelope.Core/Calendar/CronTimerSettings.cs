3094e0f83dfdf5e74af18e4e825552feb678f41a
Tomas Pilat <tomas.pilat@disig.sk>
2022-07-29 15:46:02 +0200

    added cron timer, added string helper functions

---
+++ src/Envelope.Core/Calendar/CronTimerSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace Envelope.Calendar;
+
+public class CronTimerSettings
+{
+	public string Expression { get; }
+
+	public bool IncludeSeconds { get; }
+
+	public CronExpression CronExpression { get; }
+
+	public CronTimerSettings(string expression, bool includeSeconds)
+	{
+		Expression = !string.IsNullOrWhiteSpace(expression)
+			? expression
+			: throw new ArgumentNullException(nameof(expression));
+
+		IncludeSeconds = includeSeconds;
+		CronExpression = CronExpression.Parse(expression, includeSeconds ? CronFormat.IncludeSeconds : CronFormat.Standard);
+	}
+}
